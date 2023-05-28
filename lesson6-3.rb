19799aab99374058aac1df59eb51b6cafa2f22e2
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-05-28 03:06:04 +0000

    [Update7]

--- lesson6-3.rb
+++ lesson6-3.rb
@@ -1,10 +1,26 @@
-puts "計算を始めます"
+puts "計算をはじめます"
 puts "何回繰り返しますか？"
 
 input = gets.to_i
 
 i = 1
+while i <= input do
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
 
-while i =< input do
-  puts "#{i}"
+  a = gets.to_i
+  b = gets.to_i
+
+  puts "a=#{a}"
+  puts "b=#{b}"
+
+  puts "計算結果を出力します"
+  puts "#{a}+#{b}=#{a + b}"
+  puts "#{a}-#{b}=#{a - b}"
+  puts "#{a}*#{b}=#{a * b}"
+  puts "#{a}/#{b}=#{a / b}"
+  puts "計算を終了します"
+
+  i += 1
+
 end
